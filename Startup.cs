181aa54397cb8f912cf33647bff833f286d801f8
Dimitrios Savva <dimsavva@gmail.com>
2022-08-25 14:00:50 +0200

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,73 @@
+﻿using Elsa;
+using Elsa.Persistence.EntityFramework.Core.Extensions;
+using Elsa.Persistence.EntityFramework.Sqlite;
+
+namespace ElsaDemo
+{
+    public class Startup
+    {
+        private IWebHostEnvironment Environment { get; }
+        private IConfiguration Configuration { get; }
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
+        {
+            Environment = environment;
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var elsaSection = Configuration.GetSection("Elsa");
+
+            services
+                .AddElsa(elsa => elsa
+                .UseEntityFrameworkPersistence(ef => ef.UseSqlite())
+                .AddConsoleActivities()
+                .AddHttpActivities(elsaSection.GetSection("Server").Bind)
+                .AddQuartzTemporalActivities()
+                .AddWorkflowsFrom<Startup>()
+          );
+
+            // Elsa API endpoints.
+            services.AddElsaApiEndpoints();
+
+            services.AddRazorPages();
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+
+            var elsaSection = Configuration.GetSection("Elsa");
+
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+            app
+                .UseStaticFiles() // For Dashboard.
+                .UseHttpActivities()
+                .UseRouting()
+                .UseEndpoints(endpoints =>
+                {
+                    // Elsa API Endpoints are implemented as regular ASP.NET Core API controllers.
+                    endpoints.MapControllers();
+
+                    // For Dashboard.
+                    endpoints.MapFallbackToPage("/_Host");
+                });
+
+            app.Run();
+        }
+    }
+}
